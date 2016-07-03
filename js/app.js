b762e1fec4e6b408b9588ebf64053c6690b19cb4
jkc83@mail.umkc.edu <jkc83@mail.umkc.edu>
2016-07-02 21:46:11 -0500

    .

--- js/app.js
+++ js/app.js
@@ -1,39 +1,75 @@
 // Enemies our player must avoid
 var Enemy = function() {
-    // Variables applied to each of our instances go here,
-    // we've provided one for you to get started
-
-    // The image/sprite for our enemies, this uses
-    // a helper we've provided to easily load images
+    this.x = x;
+    this.y =y;
     this.sprite = 'images/enemy-bug.png';
 };
 
-// Update the enemy's position, required method for game
-// Parameter: dt, a time delta between ticks
 Enemy.prototype.update = function(dt) {
-    // You should multiply any movement by the dt parameter
-    // which will ensure the game runs at the same speed for
-    // all computers.
+  this.x += this.speed *dt;
+  if (this.x>=505){
+    this.x=0;
+  }
+  checkCollision(this);
 };
-
 // Draw the enemy on the screen, required method for game
 Enemy.prototype.render = function() {
     ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
 };
-
 // Now write your own player class
-// This class requires an update(), render() and
-// a handleInput() method.
+var Player = function(x,y,speed) {
+  this.x = x;
+  this.y = y;
+  this.speed = speed;
+  this.sprite = 'images/char-cat-girl.jpg';
+};
 
+Player.prototype.update = function(){
 
-// Now instantiate your objects.
-// Place all enemy objects in an array called allEnemies
-// Place the player object in a variable called player
+}
 
+Player.protytype.render = function(){
+  ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
+  displayScoreLevel(score, gameLevel);
+}
 
+Player.prototype.handleInput = function(keyPress) {
+  if (keyPress=='left') {
+    player.x -=player.speed;
+  }
+  if (keyPress=='up') {
+    player.y -=player.speed -20;
+  }
+  if (keyPress=='right') {
+    player.x += player.speed;
+  }
+  if (keyPress=='down') {
+    player.y += player.speed -20;
+  }
+  console.log("KeyPress is " + keyPress);
+}
 
-// This listens for key presses and sends the keys to your
-// Player.handleInput() method. You don't need to modify this.
+var displayScoreLevel = function(aScore, aLevel) {
+  var canvas = document.getElementByTagName('canvas');
+  var firstCanvasTag = canvas[0];
+
+  scoreLevelDiv.innerHTML = 'Score'+ aScore + ' / ' + 'Level: ' + aLevel;
+  document.body.insertBefore(scoreLevelDiv, firstCanvasTag[0]);
+};
+
+var checkCollision = function(anEnemy) {
+  if (
+    player.y + 131 >= anEnemy.y +90
+    && player.x + 25 <= anEnemy.x +88
+    && player.x + 73 <= anEnemy.y = 135
+    && player.x +76 >= anEnemy.x +11) {
+      console.log('collided');
+      player.x = 202.5;
+      player.y = 383;
+    }
+  )
+}
+
 document.addEventListener('keyup', function(e) {
     var allowedKeys = {
         37: 'left',
